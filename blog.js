2013d87e37dc67c21695cba01965e913dcc120ec
J.P. Cummins <jpcummins@gmail.com>
2013-10-15 20:19:23 -0700

    Fixed path resolution issue

--- blog.js
+++ blog.js
@@ -1,10 +1,11 @@
 var fs = require('fs'),
     moment = require('moment'),
     marked = require('marked'),
-    hljs = require('highlight.js');
+    hljs = require('highlight.js'),
+    path = require('path');
 
 var entriesFilename = path.resolve(__dirname, './entries.json');
-var postsDirectory = path.resolve(__dirname, './posts/');
+var postsDirectory = path.resolve(__dirname, './posts');
 
 var loadBlogEntries = function() {
   var entries_json = fs.readFileSync(entriesFilename);
@@ -14,7 +15,7 @@ var loadBlogEntries = function() {
     var entry = entries[entry];
     entry.pub_date = new Date(entry.pub_date);
     entry.pretty_pub_date = entry.pretty_pub_date || moment(entry.pub_date).format("MMMM Do, YYYY");
-    entry.html = marked(fs.readFileSync(postsDirectory + entry.slug + '.' + entry.format, 'utf8'));
+    entry.html = marked(fs.readFileSync(path.resolve(postsDirectory, entry.slug + '.' + entry.format), 'utf8'));
   }
 
   global.entries = entries.sort(function (a, b) {
