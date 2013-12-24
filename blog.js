bc938bdcc1960b7c150ff2a9efa946d8218a6a38
J.P. Cummins <jpcummins@gmail.com>
2013-12-23 21:20:08 -0800

    Support for Twitter cards

--- blog.js
+++ blog.js
@@ -3,10 +3,29 @@ var fs = require('fs'),
     marked = require('marked'),
     hljs = require('highlight.js'),
     path = require('path'),
-    S = require('string');
+    S = require('string'),
+    request = require('request');
 
 var entriesFilename = path.resolve(__dirname, './entries.json');
 var postsDirectory = path.resolve(__dirname, './posts');
+
+var replaceTweetPermalinksWithEmbeds = function (entry, cb) {
+  var regex = /<a href=\"https:\/\/twitter\.com\/\w+\/status\/\d+\">https:\/\/twitter\.com\/\w+\/status\/(\d+)<\/a>/g;
+  var match = regex.exec(entry.html);
+
+  function makeRequest(tweetLinkHtml, tweetId, cb) {
+    request('https://api.twitter.com/1/statuses/oembed.json?align=center&omit_script=true&id=' + tweetId, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        cb(entry, tweetLinkHtml, JSON.parse(body).html);
+      }
+    });
+  }
+
+  while (match != null) {
+    makeRequest(match[0], match[1], cb);
+    match = regex.exec(entry.html);
+  }
+}
 
 var loadBlogEntries = function() {
   var entries_json = fs.readFileSync(entriesFilename);
@@ -19,13 +38,12 @@ var loadBlogEntries = function() {
     entry.pretty_pub_date = entry.pretty_pub_date || moment(entry.pub_date).format("MMMM Do, YYYY");
 
     var data = fs.readFileSync(path.resolve(postsDirectory, entry.slug + '.' + entry.format), 'utf8');
-
-    if (entry.format == 'md')
-      entry.html = marked(data);
-    else
-      entry.html = data;
-
+    entry.html = entry.format == 'md' ? marked(data) : data
     entry.summary = S(entry.html).stripTags().truncate(200);
+
+    replaceTweetPermalinksWithEmbeds(entry, function(entry, tweetLinkHtml, tweetEmbedHtml) {
+      entry.html = entry.html.replace(tweetLinkHtml, tweetEmbedHtml);
+    });
   }
 
   global.entries = entries.sort(function (a, b) {
